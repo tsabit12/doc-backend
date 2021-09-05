93a4c8bd2091b1f30be6f83e04c53f071ba5761d
Tsabit Abdul Aziz <tsabit830@gmail.com>
2021-09-05 16:51:23 +0700

    change ppn response

--- application/models/Model_reportdetail.php
+++ application/models/Model_reportdetail.php
@@ -16,7 +16,7 @@ class Model_reportdetail extends CI_Model {
         
         $q = $this->db->get();
 
-        print_r($params);
+        // print_r($params);
         if($q->num_rows() > 0){
             $result['success'] = true;
             $result['data'] = $q->result_array();
@@ -34,9 +34,10 @@ class Model_reportdetail extends CI_Model {
         $whereValues = array();
 
         $this->db->select("c.connote_code, c.connote_service as service,cc.COD, c.connote_sender_name as pengirim, cc.user, 
-                            cc.fullname as nama_pengguna,c.chargeable_weight as berat, c.connote_service_price as ongkir,  cc.PPN , cc.pph ,cc.NONPPN ,
+                            cc.fullname as nama_pengguna,c.chargeable_weight as berat, c.connote_service_price as ongkir, cc.pph ,cc.NONPPN ,
                             c.connote_surcharge_amount as asuransi, c.connote_amount as total , cc.cod_value, cc.fee_value,cc.total_cod,
                             cc.nopen, cc.regional , ldc.location_code , ldc.location_type ");
+        $this->db->select("COALESCE(cc.PPN, 0) AS ppn");
         $this->db->from('connote c');
         $this->db->join('connote_customfield cc','c.connote_id = cc.connote_id','LEFT');
         $this->db->join('location_data_created ldc','c.connote_id = ldc.connote_id','LEFT');

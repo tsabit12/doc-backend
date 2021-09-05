a51995144c8fd50dc100b4eace22c3dc852d183e
hestukarsana <hestukarsana@gmail.com>
2021-09-05 11:54:20 +0700

    add parameter

--- application/models/Model_reportdetail.php
+++ application/models/Model_reportdetail.php
@@ -2,13 +2,21 @@
 
 class Model_reportdetail extends CI_Model {
     public function getProduksi($params){
+        $state  = $params['status'];
         $result['success'] = false;
 
-        $this->query_produksi($params);
+        if ($state == '00') {
+            $this->query_produksi($params);
+        } elseif ($state == 1) {
+            $this->query_menginap($params);
+        }elseif ($state == 2) {
+            $this->query_jatuhtempo($params);
+        }       
+        
         
         $q = $this->db->get();
 
-        // print_r($q);
+        print_r($params);
         if($q->num_rows() > 0){
             $result['success'] = true;
             $result['data'] = $q->result_array();
@@ -26,7 +34,7 @@ class Model_reportdetail extends CI_Model {
         $whereValues = array();
 
         $this->db->select("c.connote_code, c.connote_service as service,cc.COD, c.connote_sender_name as pengirim, cc.user, 
-                            cc.fullname as nama_pengguna,c.connote_service_price as ongkir,  cc.PPN , cc.pph ,cc.NONPPN ,
+                            cc.fullname as nama_pengguna,c.chargeable_weight as berat, c.connote_service_price as ongkir,  cc.PPN , cc.pph ,cc.NONPPN ,
                             c.connote_surcharge_amount as asuransi, c.connote_amount as total , cc.cod_value, cc.fee_value,cc.total_cod,
                             cc.nopen, cc.regional , ldc.location_code , ldc.location_type ");
         $this->db->from('connote c');
@@ -49,42 +57,73 @@ class Model_reportdetail extends CI_Model {
         $this->db->order_by('c.connote_id', 'ASC');
     }
 
+    private function query_menginap($body){
+        $reg    = $body['regional'];
+        $kprk   = $body['kprk'];
+        $start  = $body['startdate'];
+        $end    = $body['enddate'];
 
-    // private function all($params){
-        
-    //     return $q;
-    // }
+        $whereValues = array();
 
-    // private function regional($regional,$start, $end){
-    //     $this->db->select("c.connote_code, c.connote_service as service,cc.COD, c.connote_sender_name as pengirim, cc.user, 
-    //     cc.fullname as nama_pengguna,c.connote_service_price as ongkir,  cc.PPN , cc.pph ,cc.NONPPN ,
-    //     c.connote_surcharge_amount as asuransi, c.connote_amount as total , cc.cod_value, cc.fee_value,cc.total_cod,
-    //     cc.nopen, cc.regional , ldc.location_code , ldc.location_type ");
-    //     $this->db->from('connote c');
-    //     $this->db->join('connote_customfield cc','c.connote_id = cc.connote_id','LEFT');
-    //     $this->db->join('location_data_created ldc','c.connote_id = ldc.connote_id','LEFT');
-    //     $this->db->where('cc.regional', $regional);
-    //     $this->db->where("date_format(c.created_at, '%Y-%m-%d') between '$start' and '$end'");
-    //     $this->db->order_by('c.connote_id', 'ASC');
-    //     $q = $this->db->get()->result_array();
-    //     return $q;
-    // }
+        $this->db->select("c.connote_code, c.connote_service as service,cc.COD, c.connote_sender_name as pengirim, cc.user, 
+                            cc.fullname as nama_pengguna,c.chargeable_weight as berat, c.connote_service_price as ongkir,  cc.PPN , cc.pph ,cc.NONPPN ,
+                            c.connote_surcharge_amount as asuransi, c.connote_amount as total , cc.cod_value, cc.fee_value,cc.total_cod,
+                            cc.nopen, cc.regional , ldc.location_code , ldc.location_type, c.connote_state ");
+        $this->db->from('connote c');
+        $this->db->join('connote_customfield cc','c.connote_id = cc.connote_id','LEFT');
+        $this->db->join('location_data_created ldc','c.connote_id = ldc.connote_id','LEFT');
 
-    // private function kprk($regional, $kprk,$start, $end){
-    //     $this->db->select("c.connote_code, c.connote_service as service,cc.COD, c.connote_sender_name as pengirim, cc.user, 
-    //     cc.fullname as nama_pengguna,c.connote_service_price as ongkir,  cc.PPN , cc.pph ,cc.NONPPN ,
-    //     c.connote_surcharge_amount as asuransi, c.connote_amount as total , cc.cod_value, cc.fee_value,cc.total_cod,
-    //     cc.nopen, cc.regional , ldc.location_code , ldc.location_type ");
-    //     $this->db->from('connote c');
-    //     $this->db->join('connote_customfield cc','c.connote_id = cc.connote_id','LEFT');
-    //     $this->db->join('location_data_created ldc','c.connote_id = ldc.connote_id','LEFT');
-    //     $this->db->where('cc.regional', $regional);
-    //     $this->db->where('cc.nopen', $kprk);
-    //     $this->db->where("date_format(c.created_at, '%Y-%m-%d') between '$start' and '$end'");
-    //     $this->db->order_by('c.connote_id', 'ASC');
-    //     $q = $this->db->get()->result_array();
-    //     return $q;
-    // }
+        if($reg == '00'){ //alll regional
+            
+        }else{
+            if($kprk == '00'){ //all kprk
+                $whereValues['cc.regional'] = $reg;
+            }else{
+                $whereValues['cc.nopen'] = $kprk;
+            }
+        }
+
+        $this->db->where($whereValues);
+        $this->db->where("date_format(c.created_at, '%Y-%m-%d') between '$start' and '$end'");
+        $this->db->where("c.connote_state != 'R7'" );
+
+        $this->db->order_by('c.connote_id', 'ASC');
+    }
+
+    private function query_jatuhtempo($body){
+        $reg    = $body['regional'];
+        $kprk   = $body['kprk'];
+        $start  = $body['startdate'];
+        $end    = $body['enddate'];
+
+        $whereValues = array();
+
+        $this->db->select("c.connote_code, c.connote_service as service,cc.COD, c.connote_sender_name as pengirim, cc.user, 
+                            cc.fullname as nama_pengguna,c.chargeable_weight as berat, c.connote_service_price as ongkir,  cc.PPN , cc.pph ,cc.NONPPN ,
+                            c.connote_surcharge_amount as asuransi, c.connote_amount as total , cc.cod_value, cc.fee_value,cc.total_cod,
+                            cc.nopen, cc.regional , ldc.location_code , ldc.location_type, c.connote_state ");
+        $this->db->from('connote c');
+        $this->db->join('connote_customfield cc','c.connote_id = cc.connote_id','LEFT');
+        $this->db->join('location_data_created ldc','c.connote_id = ldc.connote_id','LEFT');
+
+        if($reg == '00'){ //alll regional
+            
+        }else{
+            if($kprk == '00'){ //all kprk
+                $whereValues['cc.regional'] = $reg;
+            }else{
+                $whereValues['cc.nopen'] = $kprk;
+            }
+        }
+
+        $this->db->where($whereValues);
+        $this->db->where("date_format(c.created_at, '%Y-%m-%d') between '$start' and '$end'");
+        $this->db->where("date_format(c.connote_sla_day, '%Y-%m-%d') = date_format(now(), '%Y-%m-%d')");
+        $this->db->where("c.connote_state = 'PAID'");
+
+        $this->db->order_by('c.connote_id', 'ASC');
+    }
+
 }
 
 ?>

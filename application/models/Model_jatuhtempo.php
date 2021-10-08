b1a77a50a9e1ca40901e92ad59681df8ffe7e7b3
Tsabit Abdul Aziz <tsabit830@gmail.com>
2021-10-08 19:38:22 +0700

    debug

--- application/models/Model_jatuhtempo.php
+++ application/models/Model_jatuhtempo.php
@@ -25,6 +25,7 @@ class Model_jatuhtempo extends CI_Model {
         // }
 
         $q = $this->db->get();
+        print_r($q);
         if($q->num_rows() > 0){
             $result['exist'] = true;
             $result['result'] = $q;

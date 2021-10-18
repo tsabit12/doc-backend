2352e65eccab244d5bf152b05cc709bafb28dfb2
Tsabit Abdul Aziz <tsabit830@gmail.com>
2021-10-18 16:50:29 +0700

    Add service notif

--- application/controllers/Notification.php
+++ application/controllers/Notification.php
@@ -2,9 +2,61 @@
 require(APPPATH.'/libraries/REST_Controller.php');
 use Restserver\Libraries\REST_Controller;
 
-Class Notifaction extends REST_Controller{
+Class Notification extends REST_Controller{
+     public function __construct(){
+		parent::__construct();
+        $this->load->model('model_notification');
+    }
+
      public function index_post(){
-          
+          $res['status'] = false;
+          $res['message']['global'] = "failed push notification";
+
+          $users = $this->model_notification->getToken();
+          $summary = $this->model_notification->summary();
+
+          if($summary['success']){
+               $summarydata = $summary['data'];
+
+               $ch = curl_init();
+
+               $field = array(
+                    "to" => $users,
+                    "title" => "📬 Halo pengguna DOC",
+                    "body" => "kiriman jatuh tempo di kantor saudara sebanyak : ".$summarydata['jumlah']." items mohon segera diantar dan dilakukan update status. Terimakasih",
+                    "channelId" => "default-doc",
+                    "categoryId" => "basic",
+                    "data" => array(
+                         "title" => "Task progress reminder",
+                         "time" => $summarydata['curentdate'],
+                         "jumlah" => 40000
+                    )
+               );
+     
+               curl_setopt($ch, CURLOPT_URL, 'https://exp.host/--/api/v2/push/send');
+               curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+               curl_setopt($ch, CURLOPT_POST, 1);
+               curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($field));
+     
+               $headers = array();
+               $headers[] = 'Accept: application/json';
+               $headers[] = 'Content-Type: application/json';
+               curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+     
+               $server_output = json_decode(curl_exec($ch), true);
+               $err = curl_error($ch);
+     
+               curl_close($ch);
+     
+               if(!$err){
+                    $res['status'] = true;
+                    $res['message']['global'] = "Push notification success";
+               }
+          }
+
+          $this->response($res, 200);
+
+
      }
 }
 
---
+++ application/models/Model_notification.php
@@ -0,0 +1,31 @@
+<?php
+class Model_notification extends CI_Model {
+     public function getToken(){
+          $result = array();
+          $sql = $this->db->query("select token from users u where token <> ''");
+          $token = $sql->result_array();
+
+          foreach($token as $key){
+               $result[] = $key['token'];
+          }
+
+          return $result;
+     }
+
+     public function summary(){
+          $result['success'] = false;
+
+          $sql = $this->db->query("select count(total) as jumlah, to_char(now(), 'YYYY-MM-DD HH24:MI:SS') as curentdate
+          from summary s where periode = to_char(now(), 'YYYY-MM-DD') and types = 'jatuhtempo'");
+
+          if($sql->num_rows() > 0){
+               $data = $sql->row_array();
+               $result['data'] = $data;
+               $result['success'] = true;
+          }
+
+          return $result;
+     }
+}
+
+?>

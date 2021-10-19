82400d5ce06b286a5bda18881b063e2b57646bc8
Tsabit Abdul Aziz <tsabit830@gmail.com>
2021-10-19 13:30:24 +0700

    change time zone

--- test.php
+++ test.php
@@ -1,41 +1,8 @@
 <?php
-// The example will not work unless ob_end_clean() is called
-// on top. Strange behaviour! Would like to know a reason
-ob_end_clean();
 
-// disable all content encoding as we won't
-// be able to calculate the content-length if its enabled
-@apache_setenv('no-gzip', 1);
-@ini_set('zlib.output_compression', 0);
-@ini_set('implicit_flush', 1);
-header("Content-Encoding: none");
+date_default_timezone_set("Asia/Jakarta");
+echo date_default_timezone_get();
 
-
-// Tell client that he should close the connection
-header("Connection: close");
-
-// keep the script running even if the CLIENT closes the connection
-ignore_user_abort(); 
-
-// using ob* functions its easy to content the content-length later
-ob_start();
-
-// do your output
-echo 'hello world', PHP_EOL;
-
-
-// get the content length
-$size = ob_get_length();
-header("Content-Length: $size");
-
-// clear ob* buffers
-for ($i = 0; $i < ob_get_level(); $i++) {
-    ob_end_flush();
-}
-flush(); // clear php internal output buffer
-
-// start a time consuming task
-sleep(3);
-
+echo date('Y-m-d h:i:s', strtotime('2021-10-13T00:00:21+0700'));
 
 ?>
